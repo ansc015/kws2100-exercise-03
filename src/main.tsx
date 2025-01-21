a88fbf6101397a108fbca77f89c366e69d691d99
Anna <ansc015@egms.no>
2025-01-21 14:59:42 +0100

    laget et kart

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+import { Layer } from "ol/layer";
 
-createRoot(document.getElementById("root")!).render(<h1>Hello React</h1>);
+useGeographic();
+
+const view = new View({ center: [10.8, 59.9], zoom: 10 });
+const map = new Map({ view });
+
+function Application() {
+  const mapRef = useRef<HTMLDivElement>(null);
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+  useEffect(() => map.setTarget(mapRef.current!), []);
+  useEffect(() => map.setLayers(layers), [layers]);
+
+  return (
+    <>
+      <nav>
+        <button>Center on me</button>
+      </nav>
+      <main>
+        <div ref={mapRef}></div>
+      </main>
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(<Application />);
